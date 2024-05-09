bd1d3bfc322e0f846b56defa510302cf892ab3b7
matthiasdotsh <git@matthias.sh>
2024-05-09 14:42:23 +0200

    CPyCppyy: Workaround FileNotFound root-config

--- CPyCppyy/default.nix
+++ CPyCppyy/default.nix
@@ -24,6 +24,15 @@ python3Packages.buildPythonPackage rec {
 
   propagatedBuildInputs = [ cppyy-cling cppyy-backend];
 
+  patchPhase = ''
+    # Workaround error, that was described in cppyy-backend/default.nix
+    # See: https://github.com/wlav/CPyCppyy/blob/02072bbd818501364b37c133364e02976af4cc10/setup.py
+    # For reference line numbers
+    sed -i "43s@config_exec_args@#config_exec_args@g" setup.py
+    sed -i "44s@config_exec_args@#config_exec_args@g" setup.py
+    sed -i "45s@cli_arg@#cli_arg@g" setup.py
+    sed -i "46s|return cli_arg.decode(\"utf-8\").strip()|return \"-pthread -std=c++2a -m64\"|" setup.py
+  '';
   dontUseCmakeConfigure = true;
 
   pythonImportsCheck = [ "CPyCppyy" ];
